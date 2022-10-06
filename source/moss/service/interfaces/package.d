4b778a60d70c4c3007f39f8e642a5d0786e50ac7
Ikey Doherty <ikey@serpentos.com>
2022-10-06 12:44:32 +0100

    interfaces: New modules to define REST API for build infra
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/service/interfaces/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.service.interfaces
+ *
+ * Module level imports for interfaces
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.service.interfaces;
