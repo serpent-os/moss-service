19bec740e520a2ed50736ccd10f9780f7c85aa9c
Ikey Doherty <ikey@serpentos.com>
2022-10-06 12:06:49 +0100

    Initial source inclusion
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/service/package.d
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss-service
+ *
+ * Module level imports for moss-service
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.service;
+
+public import vibe.d;
