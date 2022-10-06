4d57cfd9ef2fb3ac97ea4244532ae9901a99cf3d
Ikey Doherty <ikey@serpentos.com>
2022-10-06 12:20:37 +0100

    models: Start new package
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/service/models/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.service.models
+ *
+ * Module level imports for models
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.service.models;
