eba63b71b995d83c757da6ea6323377366c6f785
Ikey Doherty <ikey@serpentos.com>
2022-11-14 22:34:22 +0000

    models: Add new, simplified, BearerToken type
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/service/models/bearertoken.d
@@ -0,0 +1,46 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.service.models.bearertoken
+ *
+ * Persistence for Bearer Tokens
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.service.models.bearertoken;
+
+public import moss.db.keyvalue.orm;
+public import moss.service.models.user : UserIdentifier;
+public import std.stdint : uint64_t;
+
+/**
+ * BearerToken stores the remote-access token required for
+ * a service account to authorize against the system DB
+ *
+ * This token is generated locally and verifed using our
+ * local public key, and is issued for the purpose of remote
+ * service accounts (summit <-> avalanche)
+ */
+public @Model struct BearerToken
+{
+    /**
+     * User ID
+     */
+    @PrimaryKey UserIdentifier id;
+
+    /**
+     * Raw token string
+     */
+    string rawToken;
+
+    /**
+     * When does the token expire?
+     */
+    uint64_t expiryUTC;
+}
