562b8eda491f304460cd44c234eb8614f116e924
Ikey Doherty <ikey@serpentos.com>
2022-11-15 23:59:08 +0000

    accounts/auth: Add some nice role helpers
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/service/accounts/auth.d
+++ source/moss/service/accounts/auth.d
@@ -18,7 +18,18 @@ module moss.service.accounts.auth;
 import vibe.d;
 
 import moss.service.accounts.manager;
+import moss.service.tokens;
 import moss.service.tokens.manager;
+import moss.service.models.account;
+
+private enum AccessMode
+{
+    None = 0,
+    BearerToken = 1 << 1,
+    AccessToken = 1 << 2,
+    WebConnection = 1 << 3,
+    APIConnection = 1 << 4
+};
 
 /**
  * Attempt to determine authentication from the current web context
@@ -34,6 +45,67 @@ public struct AccountAuthentication
     {
         throw new HTTPStatusException(HTTPStatus.forbidden, "no permissions implemented sorry");
     }
+
+    /**
+     * Returns: true if a bearer token is being used
+     */
+    pure @property bool isBearerToken() @safe @nogc nothrow const
+    {
+        return (mode & AccessMode.BearerToken) == AccessMode.BearerToken;
+    }
+
+    /**
+     * Returns: true if an access token is being used
+     */
+    pure @property bool isAccessToken() @safe @nogc nothrow const
+    {
+        return (mode & AccessMode.AccessToken) == AccessMode.AccessToken;
+    }
+
+    /**
+     * Returns: true if a web connection is being used
+     */
+    pure @property bool isWeb() @safe @nogc nothrow const
+    {
+        return (mode & AccessMode.WebConnection) == AccessMode.WebConnection;
+    }
+
+    /**
+     * Returns: true if an API connection (REST) is being used
+     */
+    pure @property bool isAPI() @safe @nogc nothrow const
+    {
+        return (mode & AccessMode.APIConnection) == AccessMode.APIConnection;
+    }
+
+    /**
+     * Returns: true if the token is for a service account
+     */
+    pure @property bool isServiceAccount() @safe @nogc nothrow const
+    {
+        return accountType == AccountType.Service;
+    }
+
+    /**
+     * Returns: true if the token is for a user account
+     */
+    pure @property bool isUserAccount() @safe @nogc nothrow const
+    {
+        return accountType == AccountType.Standard;
+    }
+
+    /**
+     * Returns: true if the token is for a bot account
+     */
+    pure @property bool isBotAccount() @safe @nogc nothrow const
+    {
+        return accountType == AccountType.Bot;
+    }
+
+private:
+
+    AccessMode mode = AccessMode.None;
+    AccountType accountType = AccountType.Bot;
 }
 
 /**
