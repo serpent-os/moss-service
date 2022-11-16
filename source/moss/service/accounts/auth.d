0fa54fa2a2d3f97435dd0b3db0f72f6903e29aa6
Ikey Doherty <ikey@serpentos.com>
2022-11-16 01:06:29 +0000

    accounts/auth: Fairly comprehensive authentication handler
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/service/accounts/auth.d
+++ source/moss/service/accounts/auth.d
@@ -22,13 +22,16 @@ import moss.service.tokens;
 import moss.service.tokens.manager;
 import moss.service.models.account;
 
+import std.string : strip, split;
+
 private enum AccessMode
 {
     None = 0,
     BearerToken = 1 << 1,
     AccessToken = 1 << 2,
     WebConnection = 1 << 3,
-    APIConnection = 1 << 4
+    APIConnection = 1 << 4,
+    Expired = 1 << 5,
 };
 
 /**
@@ -43,7 +46,65 @@ public struct AccountAuthentication
     this(scope return AccountManager accountManager, scope return TokenManager tokenManager,
             scope HTTPServerRequest req, scope HTTPServerResponse res) @safe
     {
-        throw new HTTPStatusException(HTTPStatus.forbidden, "no permissions implemented sorry");
+        string encodedToken;
+
+        /* Try headers first */
+        string authHeader = req.headers.get("Authorization", null);
+        if (authHeader !is null)
+        {
+            auto tokens = authHeader.split();
+            enforceHTTP(tokens.length == 2, HTTPStatus.badRequest,
+                    "Invalid bearer token assignment");
+            immutable method = tokens[0].strip();
+            immutable payload = tokens[1].strip();
+
+            enforceHTTP(method == "Bearer", HTTPStatus.forbidden,
+                    "Only Bearer scheme supported for auth");
+            encodedToken = payload;
+            mode = AccessMode.APIConnection;
+        }
+        else if (req.session)
+        {
+            encodedToken = req.session.get!string("accessToken", null);
+            mode = AccessMode.WebConnection;
+        }
+
+        /* MUST have a token by now .. */
+        enforceHTTP(encodedToken !is null, HTTPStatus.forbidden, "You must supply a valid token");
+
+        /* Decode what we found */
+        Token.decode(encodedToken).match!((Token tk) { connectionToken = tk; }, (TokenError e) {
+            throw new HTTPStatusException(HTTPStatus.forbidden, e.message);
+        });
+
+        /* Now verify that *we created it* */
+        enforceHTTP(tokenManager.verifyOurs(connectionToken),
+                HTTPStatus.forbidden, "Supplied token is not valid for this instance");
+
+        accountType = connectionToken.payload.act;
+
+        /* Set the purpose */
+        switch (connectionToken.payload.purpose)
+        {
+        case TokenPurpose.Authorization:
+            mode |= AccessMode.BearerToken;
+            break;
+        case TokenPurpose.Authentication:
+            mode |= AccessMode.AccessToken;
+            break;
+        default:
+            throw new HTTPStatusException(HTTPStatus.badRequest, "Invalid token purpose");
+        }
+
+        /* Ensure timing is good */
+        immutable timeNow = Clock.currTime(UTC()).toUnixTime();
+        if (timeNow >= connectionToken.payload.exp)
+        {
+            mode |= AccessMode.Expired;
+        }
+
+        /* Stash the connection token */
+        () @trusted { req.context["token"] = connectionToken; }();
     }
 
     /**
@@ -102,10 +163,27 @@ public struct AccountAuthentication
         return accountType == AccountType.Bot;
     }
 
+    /**
+     * Returns: true if the token is expired
+     */
+    pure @property bool isExpired() @safe @nogc nothrow const
+    {
+        return (mode & AccessMode.Expired) == AccessMode.Expired;
+    }
+
+    /**
+     * Returns: true if the token is NOT expired
+     */
+    pure @property bool isNotExpired() @safe @nogc nothrow const
+    {
+        return !isExpired();
+    }
+
 private:
 
     AccessMode mode = AccessMode.None;
     AccountType accountType = AccountType.Bot;
+    Token connectionToken;
 }
 
 /**
