06927254bc9ad2e430df03550c12e868534b4429
Ikey Doherty <ikey@serpentos.com>
2023-02-06 23:17:26 +0000

    interfaces: Absorb vessels notion of collectables
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/service/interfaces/package.d
+++ source/moss/service/interfaces/package.d
@@ -18,3 +18,46 @@ module moss.service.interfaces;
 public import moss.service.interfaces.avalanche;
 public import moss.service.interfaces.endpoints;
 public import moss.service.interfaces.summit;
+
+/**
+ * Collectable assets passed between the infrastructure
+ */
+public enum CollectableType : string
+{
+    /**
+     * A build log file
+     */
+    Log = "log",
+
+    /**
+     * The build manifest
+     */
+    Manifest = "manifest",
+
+    /**
+     * A binary package
+     */
+    Package = "package",
+}
+
+/**
+ * An asset for passing between infrastructure components,
+ * such as a package or log file
+ */
+public struct Collectable
+{
+    /**
+     * Type of collectable
+     */
+    CollectableType type;
+
+    /**
+     * Fully qualified URL, must be set correctly by each component
+     */
+    string uri;
+
+    /**
+     * Hash sum for the asset
+     */
+    string sha256sum;
+}
