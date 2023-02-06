b005f97427f5238f2f5ca31f20f7b1d84ce05eac
Ikey Doherty <ikey@serpentos.com>
2023-02-06 23:21:26 +0000

    interfaces/summit: Require Collectables for API
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/service/interfaces/summit.d
+++ source/moss/service/interfaces/summit.d
@@ -18,6 +18,7 @@ module moss.service.interfaces.summit;
 public import std.stdint : uint64_t;
 
 public import moss.service.tokens : NullableToken;
+public import moss.service.interfaces : Collectable;
 import moss.service.accounts.auth : retrieveToken;
 import vibe.d;
 import vibe.web.auth;
@@ -41,17 +42,27 @@ import vibe.web.auth;
                 ".authenticate() method should have been overriden!");
     }
 
-    /**
+    /** 
      * Inform the dashboard that the build failed
+     *
+     * Params:
+     *   taskID = Unique task identifier
+     *   collectables = Should include at least the log file
+     *   token = Set by middleware
      */
     @before!retrieveToken("token") @path("buildFailed") @method(HTTPMethod.POST)
     @auth(Role.notExpired & Role.API & Role.serviceAccount & Role.accessToken)
-    void buildFailed(uint64_t taskID, NullableToken token) @safe;
+    void buildFailed(uint64_t taskID, Collectable[] collectables, NullableToken token) @safe;
 
-    /**
+    /** 
      * Inform the dashboard that the build succeeded
+     *
+     * Params:
+     *   taskID = Unique task identifier
+     *   collectables = Needs to include the log file, and all stones
+     *   token = Set by middleware
      */
     @before!retrieveToken("token") @path("buildSucceeded") @method(HTTPMethod.POST)
     @auth(Role.notExpired & Role.API & Role.serviceAccount & Role.accessToken)
-    void buildSucceeded(uint64_t taskID, NullableToken token) @safe;
+    void buildSucceeded(uint64_t taskID, Collectable[] collectables, NullableToken token) @safe;
 }
